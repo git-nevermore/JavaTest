b3f09adadbd240c9cefee6b1e2abb131c41017f2
NEVERMORE <nevermore_2016@foxmail.com>
2017-11-14 10:05:21 +0800

    '中文测试'

---
+++ src/com/test/git/GitBranchTest.java
@@ -0,0 +1,16 @@
+package com.test.git;
+
+import org.junit.Test;
+
+/**
+ * Git Branch Test
+ * @author chenwenjie
+ * @since 2017-11-14
+ */
+public class GitBranchTest {
+
+	@Test
+	public void branchTest() {
+		System.out.println("Master");
+	}
+}
